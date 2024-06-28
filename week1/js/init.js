5746ab2ddcb48a583c083b6ff5b5c387a6a5b9d7
Sadhana Jeyakumar <sjeyakum@gmail.com>
2024-06-28 15:52:29 -0700

    new changes

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,12 +1,63 @@
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL', // Your style URL
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
     center: [-118.444, 34.0709], // Starting position [lng, lat]
     zoom: 15,
     pitch: 45,
     bearing: -17.6,
     antialias: true
+});
+
+const MAPTILER_KEY = 'wsyYBQjqRwKnNsZrtci1';
+
+map.on('load', () => {
+    // Insert the layer beneath any symbol layer.
+    const layers = map.getStyle().layers;
+
+    let labelLayerId;
+    for (let i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+        }
+    }
+
+    map.addSource('openmaptiles', {
+        url: `https://api.maptiler.com/tiles/v3/tiles.json?key=${MAPTILER_KEY}`,
+        type: 'vector',
+    });
+
+    map.addLayer(
+        {
+            'id': '3d-buildings',
+            'source': 'openmaptiles',
+            'source-layer': 'building',
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'render_height'], 0, 'lightgray', 200, 'royalblue', 400, 'lightblue'
+                ],
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    16,
+                    ['get', 'render_height']
+                ],
+                'fill-extrusion-base': ['case',
+                    ['>=', ['get', 'zoom'], 16],
+                    ['get', 'render_min_height'], 0
+                ]
+            }
+        },
+        labelLayerId
+    );
 });
 
 // Add a marker to the map
@@ -28,3 +79,88 @@ new maplibregl.Marker()
         .setHTML('<div><b>Northern Lights Café</b><br/><img src="https://png.pngtree.com/png-clipart/20230917/original/pngtree-sandwich-icon-icon-illustration-vector-png-image_12290588.png" width="100"/><br/>It is convenient that the café is located right next to YRL. I love their chicken pesto panini!</div>'))
     .addTo(map);
 
+map.on('load', async () => {
+    // Music Café: Cinnamon Roll Icon
+    let image = await map.loadImage('https://cdn-icons-png.flaticon.com/256/10775/10775777.png');
+    map.addImage('cin-roll', image);
+    map.addSource('point-cin-roll', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [-118.441978, 34.070290]
+                    }
+                }
+            ]
+        }
+    });
+    map.addLayer({
+        'id': 'points-cin-roll',
+        'type': 'symbol',
+        'source': 'point-cin-roll',
+        'layout': {
+            'icon-image': 'cin-roll',
+            'icon-size': 0.75
+        }
+    });
+
+    // Yoshinoya: Chicken Teriyaki Bowl Icon
+    image = await map.loadImage('https://static.vecteezy.com/system/resources/previews/036/085/711/original/ai-generated-chicken-teriyaki-bowl-on-transparent-background-png.png');
+    map.addImage('bowl', image);
+    map.addSource('point-bowl', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [-118.440530, 34.067660]
+                    }
+                }
+            ]
+        }
+    });
+    map.addLayer({
+        'id': 'points-bowl',
+        'type': 'symbol',
+        'source': 'point-bowl',
+        'layout': {
+            'icon-image': 'bowl',
+            'icon-size': 0.75
+        }
+    });
+
+    // Northern Lights Café: Sandwich Icon
+    image = await map.loadImage('https://png.pngtree.com/png-clipart/20230917/original/pngtree-sandwich-icon-icon-illustration-vector-png-image_12290588.png');
+    map.addImage('panini', image);
+    map.addSource('point-panini', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [-118.442370, 34.075330]
+                    }
+                }
+            ]
+        }
+    });
+    map.addLayer({
+        'id': 'points-panini',
+        'type': 'symbol',
+        'source': 'point-panini',
+        'layout': {
+            'icon-image': 'panini',
+            'icon-size': 0.75
+        }
+    });
+});
